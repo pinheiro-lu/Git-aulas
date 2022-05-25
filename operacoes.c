edb67324bd229d982d930f291f131dd87fccf9b0
Luan Pereira Pinheiro <luan@kali.kali>
2022-05-25 11:32:21 -0300

    Implementação do menu principal do projeto de operações matemáticas

---
+++ operacoes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+Programa para realizar as quatro operações matemáticas.
+*/
+
+
+int main () {
+	int operacao = 1;
+
+	while (operacao != 0) {
+		printf("===== MENU PRINCIPAL =====\n");
+		printf("01 - Adição\n");
+		printf("02 - Subtração\n");
+		printf("03 - Multiplicação\n");
+		printf("04 - Divisão\n");
+		printf("00 - Sair\n");
+		printf("Escolha uma operação: ");
+	}
+}
